62c67a4aa149d4d5532fc09e60712c2145ed2bc3
you <youfeng@haizhi.com>
2017-04-12 23:20:00 +0800

    代码调整

--- intro/hello.c
+++ intro/hello.c
@@ -2,6 +2,11 @@
 
 int
 main(void) {
+    printf("long size = %ld\n", sizeof(long));
+    printf("double size = %ld\n", sizeof(double));
+    printf("int size = %ld\n", sizeof(int));
+    printf("char size = %ld\n", sizeof(char));
+    /* printf("max double = %lf\n", ); */
     printf("hello world from process ID %ld\n", (long) getpid());
     exit(0);
 }
